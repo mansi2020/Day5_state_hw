c13b8a050a319f3387ffb18932610ccba36da1de
mansi2020 <sorathiyamansi11@gmail.com>
2023-12-24 00:11:28 +0530

    add animation and style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 import Paradata from "./Data.json";
+import Header from "./Component/Header";
+
 
 function App() {
   // state variable poaranum and paragraphs
@@ -14,38 +16,58 @@ function App() {
 
   // function when click on generate
   let onClickParaGenerate = () => {
-    const generatedParagraphs = [];
-    for (let i = 0; i < paraNum; i++) {
-      generatedParagraphs.push(<p key={i}>{i + 1}{") "}{Paradata[i].Paragraph}</p>);
-    }
-    setparagraphs(generatedParagraphs);
-    if(paraNum==0){
-      {alert(" Ayyo ! you know that you wrote negative input  😵😵")}
+    if (paraNum == 0) {
+      {
+        alert(" Ayyo ! you know that you wrote negative input  😵😵");
+      }
+    } else if (paraNum >= 10) {
+      {
+        alert(
+          " Ayyo ! you are demanding very much paragraph in one go, kindly take little litlle 😀"
+        );
+      }
+    } else {
+
+      // add paragraph according to paranum
+      const generatedParagraphs = [];
+      for (let i = 0; i < paraNum; i++) {
+        generatedParagraphs.push(
+          <p key={i}>
+            {i + 1}
+            {") "}
+            {Paradata[i].Paragraph}
+          </p>
+        );
+      }
+      setparagraphs(generatedParagraphs);
     }
   };
 
   return (
-    <div>
-      <h1>TIRED OF BORING LOREM IPSUM?</h1>
-      <p>Paragraphs:</p>
-      <input
-        type="number"
-        onChange={(e) => onChangeInput(e, setParaNum)}
-        value={paraNum}
-      ></input>
-      <button onClick={onClickParaGenerate}>GENERATE</button>
+    <div className="App_conatiner">
+      <Header></Header>
+      <div className="App_conatiner_content">
+        <h1 className="app_heading">TIRED OF BORING LOREM IPSUM?</h1>
+        <div>
+          <p>Paragraphs:</p>
+          <input
+            type="number"
+            onChange={(e) => onChangeInput(e, setParaNum)}
+            value={paraNum}
+          ></input>
+        </div>
+        <button onClick={onClickParaGenerate}>GENERATE</button>
+      </div>
 
       {/* show paragraph acoording given num*/}
-      {(paragraphs.length > 0 && (
-        <div>
+      {paragraphs.length > 0 && (
+        <div className="app_para_container">
           <h3>Generated Paragraphs:</h3>
           {paragraphs.map((paragraph, index) => (
-            <div key={index}>{paragraph}</div>
+            <div key={index} className="App_conatainer_gerated_para">{paragraph}</div>
           ))}
         </div>
-      ))}
-
-      {/*  */}
+      )}
     </div>
   );
 }

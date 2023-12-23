a795bf77f908fa2511ce7dd7fd39b8cfae552cd9
mansi2020 <sorathiyamansi11@gmail.com>
2023-12-23 22:48:19 +0530

    add functionality for assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Paradata from "./Data.json";
 
 function App() {
+  // state variable poaranum and paragraphs
+  const [paraNum, setParaNum] = useState(0);
+  const [paragraphs, setparagraphs] = useState([]);
+
+  // Change the No of input
+  let onChangeInput = (e) => {
+    setParaNum(e.target.value);
+  };
+
+  // function when click on generate
+  let onClickParaGenerate = () => {
+    const generatedParagraphs = [];
+    for (let i = 0; i < paraNum; i++) {
+      generatedParagraphs.push(<p key={i}>{i + 1}{") "}{Paradata[i].Paragraph}</p>);
+    }
+    setparagraphs(generatedParagraphs);
+    if(paraNum==0){
+      {alert(" Ayyo ! you know that you wrote negative input  😵😵")}
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TIRED OF BORING LOREM IPSUM?</h1>
+      <p>Paragraphs:</p>
+      <input
+        type="number"
+        onChange={(e) => onChangeInput(e, setParaNum)}
+        value={paraNum}
+      ></input>
+      <button onClick={onClickParaGenerate}>GENERATE</button>
+
+      {/* show paragraph acoording given num*/}
+      {(paragraphs.length > 0 && (
+        <div>
+          <h3>Generated Paragraphs:</h3>
+          {paragraphs.map((paragraph, index) => (
+            <div key={index}>{paragraph}</div>
+          ))}
+        </div>
+      ))}
+
+      {/*  */}
     </div>
   );
 }
---
+++ src/Para.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Data from './Data.json'
+
+const Para = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Para
